c84ada9d3a29553cf43a3c47aa2c794d5e2babad
Ana Carolina de Andrade Silva <129069189+anamelcarolina@users.noreply.github.com>
2025-01-09 20:49:57 -0300

    Update Console.java

--- Console.java
+++ Console.java
@@ -18,13 +18,28 @@ public class Console {
          
          System.out.printf("Salario: %.3f%n", 1234.5679);
          System.out.printf("Nome: %s%n", "Carol");
+         //System.OUT: saida do sistema, é o que vai imprimir no computador (é a tela)
          
+                                       //Scanner
+                                       
+     // O tipo:  o nome: recebe    valor:
+         Scanner entrada = new Scanner(System.in);// System.in é uma "porta" que permite ao programa receber dados do teclado. Scanner Ajuda a "ler" o que a pessoa digitar no teclado
+          
+          System.out.print("Digite o seu nome: ");
+          String nome = entrada.nextLine();// O programa espera o usuario digitar algo
+          System.out.println("\nNome= " + nome);
+          
+          System.out.print("\nDigite seu sobrenome: ");
+          String sobrenome = entrada.nextLine();
+          System.out.println("Sobrenome" + sobrenome);
+          System.out.println( "Seja bem vindo(a): " + nome + sobrenome );
+        
+         System.out.print("\nDigite a sua idade: ");
+         int idade = entrada.nextInt();
+         System.out.println("Confirmando, a sua idade e:  " + idade);
          
-       //                                
-         Scanner entrada = new Scanner(System.in); // System.in é uma "porta" que permite ao programa receber dados do teclado. Scanner Ajuda a "ler" o que a pessoa digitar no tclado
-               
-         
-         
-         
-   }
+          entrada.close();// Para fechar o Scanner
+    }
+
+   
 }    
