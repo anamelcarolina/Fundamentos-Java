50677b5afa426cbdb5ca2a3293160d59705def0d
Ana Carolina de Andrade Silva <129069189+anamelcarolina@users.noreply.github.com>
2025-01-09 14:46:39 -0300

    Create Console.java

---
+++ Console.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fundamentos;
+
+import java.util.Scanner;// Import do Scanner
+
+public class Console {
+   public static void main(String[] args){
+         //Print vai imprir tudo na mesma linha. Saida "Bom dia!!"
+         System.out.print("Bom");   
+         System.out.print(" dia!!\n");// \n para pular linha
+         
+         //Println vai pular uma linha e vai imprimir 
+         System.out.println("Eu gosto de chocolate");
+         System.out.println("Receitas");
+         
+         System.out.printf("Salario: %.3f%n", 1234.5679);
+         System.out.printf("Nome: %s%n", "Carol");
+         
+         
+       //                                
+         Scanner entrada = new Scanner(System.in); // System.in é uma "porta" que permite ao programa receber dados do teclado. Scanner Ajuda a "ler" o que a pessoa digitar no tclado
+               
+         
+         
+         
+   }
+}    
