4dc2a624094127f36ab51b9fca285f46e1e64e91
Ana Carolina de Andrade Silva <129069189+anamelcarolina@users.noreply.github.com>
2025-01-15 14:59:59 -0300

    Update Wrappers.java

--- Wrappers.java
+++ Wrappers.java
@@ -3,35 +3,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.fundamentos;
+/*                                                                      Objeto vs Primitivos
 
+*Tipos Primitivos: int, double, boolean, etc... são dados simples. Voce não pode pedir para fazer algo (metodos) como: "Me mostre quantos digitos voce tem" ou "converta-se para texto"
+
+*Objetos: tem metodos, o que significa que voce pode pedir para eles fazerem coisas especificias, igual eu citei lá em cima. Os Wrappers transformam os primitivos em objetos 
+
+Exs: 
+*Primitivos (int)
+
+int numero = 10;
+numero.toString(); --> transformar em texto
+Resultado: Erro
+
+*Wrapper (Integer)
+
+Integer numero = 10;
+Systm.out.println(numero.toString()); --> Saida 10
+
+*/
 public class Wrappers {
-    public static void main (String[] args){
-    //byte
-    Byte b = 100;
-    Short s = 1000;
+     public static void main (String[] args){
+         
+         //byte
+         Byte b = 100;//Wrapper para o tipo byte
+         System.out.println(b.byteValue());// Retorna o valor do ojeto Byte como um tipo primitivo byte
+         
+         Short s = 1000;//Wrapper para o tipo short
+         System.out.println(s.toString()); //Transforma em texto, em String
+         
+         
+         Integer i = Integer.parseInt("10000");// Converte para int
+         System.out.println(i * 3);// Saida 30000
+         
+         
+         Long l = 1000000L;// adc um L no final 
+         System.out.println(l / 3);
+         
+         Boolean bo = Boolean.parseBoolean("true");// Convertando string por boolean
+         System.out.println(bo);
+         System.out.println(bo.toString(). toUpperCase());// boolean convertido para String  
+         
+         
+         Float f = 123.10F;
+         System.out.println(f);
     
-    // Integer.parseInt(entrada.next());
-    Integer i = 10000;
-    Long l = 100000L;
+        Double d = 1234.5678;
+        System.out.println(d);
     
-    System.out.println(b.byteValue());
-    System.out.println(s.toString());
-    System.out.println(i * 3);
-    System.out.println(l / 3);
-      
-    Float f = 123.10F;
-    System.out.println(f);
-    
-    Double d = 1234.5678;
-    System.out.println(d);
-    
-    Boolean bo = Boolean.parseBpplean("true");
-    System.out.println(bo);
-    System.out.println(bo.toString().toUpperCase());
-    
-    
-    Character c = '#'; //char
-    System.out.println(c + "...");
-    }
-    
+    }   
 }
+
