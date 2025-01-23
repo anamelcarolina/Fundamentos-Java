b6e9cb347eb461af0a2bfa2de98dcff89cdf9ddf
Ana Carolina de Andrade Silva <129069189+anamelcarolina@users.noreply.github.com>
2025-01-23 11:16:38 -0300

    Create OperadorAritmeticos.java

---
+++ OperadorAritmeticos.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fundamentos;
+
+public class OperadoresAritmeticos {
+    public static void main (String[] args){
+    System.out.println(2 + 3);
+    
+    var x = 34.56;
+    double y = 2.2;
+    
+    System.out.println(x + y);// Adc
+    System.out.println(x - y);// Menos
+    System.out.println(x * y);//Multiplicação
+    System.out.println(x / y);//Divisão
+    
+    /*  Saida:
+    36.760000000000005
+    32.36
+    76.03200000000001
+    15.709090909090909
+    */
+    
+    int a = 8;
+    int b = 3;
+    
+    System.out.println(a + b);// Adc
+    System.out.println(a - b);// Menos
+    System.out.println(a * b);//Multiplicação
+    System.out.println(a / b);//Divisão
+    System.out.println(a / (double) b);// Converter para double
+    System.out.println(a % b );// Resto da divisão. Operador Modulo
+    
+    /*  Saida:
+    11
+    5
+    24
+    2
+    2.6666666666666665
+    2
+    */
+    
+    System.out.println(x + y - a * b);// Saida: 12.760000000000005
+    }
+    
+}
